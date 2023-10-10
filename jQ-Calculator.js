47ed4c5f5171f4c65c1c2aa6ca730d18382294d5
Timothy <128176649+Timothy-P@users.noreply.github.com>
2023-10-10 19:33:49 +0000

    Changes :D

--- jQ-Calculator.js
+++ jQ-Calculator.js
@@ -1,1 +1,20 @@
-
+function A() {
+    var B = document.getElementById("calcinput");
+    let expression = document.getElementById("calcinput").value;
+    let result;
+    try {
+        expression = expression.replace(/sin\(/g, 'sin(' + Math.PI / 180 + '*');
+        expression = expression.replace(/cos\(/g, 'cos(' + Math.PI / 180 + '*');
+        expression = expression.replace(/tan\(/g, 'tan(' + Math.PI / 180 + '*');
+        result = math.evaluate(expression);
+        document.getElementById("calcinput").value = result;
+    } catch (error) {
+        document.getElementById("calcinput").value = "Error";
+    }
+    var C = document.getElementById("calcinput");
+    const para = document.createElement("p");
+    const node = document.createTextNode(B + "=" + C);
+    para.appendChild(node);
+    const element = document.getElementById("history");
+    element.appendChild(para);
+};
